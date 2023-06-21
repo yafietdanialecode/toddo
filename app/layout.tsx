949d244f0c8c68864708213c0fe2b5106847846c
Yafiet Daniale <yafietdaniale@gmail.com>
2023-06-21 19:48:14 +0300

    added favicon and some site.manifest
    added some manifest file and some site manifest file and also linked it with public/layout.tsx and added head tag with some meta informations in it

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Home',
-  description: 'Toddo app home page',
+  description: 'Toddo app home page'
 }
 
 export default function RootLayout({
@@ -15,6 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
